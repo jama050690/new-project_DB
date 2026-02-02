906817856041594a7655b9d473d2d2d425ba8a47
Jamshiddin <jbm050690@gmail.com>
2026-02-02 20:11:31 +0500

    ikkita worker functioni bittada ishlatish kodini yodim

--- vite-project/src/Worker.js
+++ vite-project/src/Worker.js
@@ -2,20 +2,25 @@ self.onmessage = (event) => {
   if (event.data.type === "doHeavyTask2") {
     self.postMessage({
       type: "doHeavyTask2",
-      result: doHeavyTask2(event.data.interations),
+      result: doHeavyTask2(event.data.iterations),
+    });
+  } else if (event.data.type === "doHeavyTask") {
+    self.postMessage({
+      type: "doHeavyTask",
+      result: doHeavyTask(event.data.iterations),
     });
   }
 };
-function doHeavyTask(interations) {
+function doHeavyTask(iterations) {
   let sum = 0;
-  for (let i = 0; i < interations; i++) {
+  for (let i = 0; i < iterations; i++) {
     sum += i;
   }
   return sum;
 }
-function doHeavyTask2(interations) {
+function doHeavyTask2(iterations) {
   let sum = 1;
-  for (let i = 1; i < interations; i++) {
+  for (let i = 1; i < iterations; i++) {
     sum *= i;
   }
   return sum;
--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,15 +1,23 @@
 import BG_THREAD from "./Worker.js?worker";
-const button = document.querySelector("button");
+const button = document.querySelector(".button");
 const h1 = document.querySelector("h1");
+const button1 = document.querySelector(".button1");
 const bg = new BG_THREAD();
 bg.onmessage = (event) => {
-  if (event.data.type === "doHeavyTask2") {
+  if (event.data.type === "doHeavyTask2" || event.data.type === "doHeavyTask") {
     h1.textContent = event.data.result;
   }
 };
+
 button.onclick = () => {
   bg.postMessage({
     type: "doHeavyTask2",
-    interations: 5e1,
+    iterations: 5e3,
+  });
+};
+button1.onclick = () => {
+  bg.postMessage({
+    type: "doHeavyTask",
+    iterations: 5e7,
   });
 };
